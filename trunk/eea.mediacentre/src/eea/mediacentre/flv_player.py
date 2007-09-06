bd50b07039eea26f43307c9c12d7ea6bacc3eac6
terletim <terletim@24821446-e7ea-0310-81ac-b189f4507b0b>
2007-09-06 21:15:18 +0000

    The flv player adapter is moved from Plone4ArtistsVideo to eea.mediacentre. Now we don't need our own Plone4ArtistsVideo product anymore in the buildout.

---
+++ trunk/eea.mediacentre/src/eea/mediacentre/flv_player.py
@@ -0,0 +1,74 @@
+from zope import interface
+from zope import component
+from p4a.video import interfaces
+from Products.CMFCore import utils as cmfutils
+from p4a.fileimage.image._widget import ImageURLWidget
+
+class FLVVideoPlayer(object):
+    interface.implements(interfaces.IMediaPlayer)
+    component.adapts(object)
+
+    def __init__(self, context):
+        self.context = context
+        # default we want the videos in their original size
+        self.max_width = 10000
+        self.max_height = 10000
+
+    def __call__(self, downloadurl, imageurl):
+        import pdb; pdb.set_trace()
+        showUrl = True
+        if downloadurl:
+            contentobj = self.context.context.context
+            showUrl = False
+        else:
+            contentobj = self.context
+
+        portal_tool = cmfutils.getToolByName(contentobj, 'portal_url')
+        portal_url = portal_tool.getPortalObject().absolute_url()
+
+        player = portal_url + "/++resource++flowplayer/FlowPlayerThermo.swf"
+        fullscreenUrl = portal_url + "/++resource++flowplayer/fullscreen.js"
+        
+        title = contentobj.title
+
+        videoobj = interfaces.IVideo(contentobj)
+        width = int(float(videoobj.width))
+        height = int(float(videoobj.height))
+
+        # videos shown as "related multimedia" should only show as 180x135
+        if width > self.max_width or height > self.max_height:
+            width_diff = float(max(width - self.max_width, 0))/width
+            height_diff = float(max(height - self.max_height, 0))/height
+            diff = max(width_diff, height_diff)
+            if diff > 0:
+                width = width * (1-diff)
+                height = height * (1-diff)
+
+        downloadurl = contentobj.absolute_url()
+        playlist = "[ { url: '%s/video-image.jpg'}, " % portal_url
+        playlist += "{ url: '%s' } ]" % downloadurl 
+        
+        return """
+        <div class="flowplayer">
+            <div id="video%(videoid)s" class="embeddedvideo">
+              Please enable javascript to watch video.
+            </div>
+            <script type="text/javascript">
+              // <![CDATA[
+              var so = new SWFObject("%(player)s?config={ playList: %(playlist)s, scaleSplash: true, initialScale: 'scale', showFullScreenButton: false }", "FlowPlayer", "%(width)s", "%(height)s", "7", "#ffffff");
+                 so.addParam("AllowScriptAccess", "always");
+                 so.addParam("wmode", "transparent");
+                 so.write("video%(videoid)s");
+              // ]]>
+            </script>     
+        </div>
+
+        """ % {'videoid': contentobj.getId().replace('.','-'),
+               'player': player,
+               'title': title,
+               'width': width,
+               'height': height+16,
+               'videoUrl': downloadurl + '/view',
+               'playlist': playlist,
+               'fullscreenUrl': fullscreenUrl,
+               'showUrl': showUrl}
