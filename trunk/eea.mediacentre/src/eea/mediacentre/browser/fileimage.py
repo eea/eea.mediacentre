adc02dbc62a6fa490ba7833dd36009e20fd69721
terletim <terletim@24821446-e7ea-0310-81ac-b189f4507b0b>
2007-10-17 16:16:26 +0000

    p4a changed method name for retrieving the image url, so our overriding method must change name as well. This fixes #5113.

--- trunk/eea.mediacentre/src/eea/mediacentre/browser/fileimage.py
+++ trunk/eea.mediacentre/src/eea/mediacentre/browser/fileimage.py
@@ -2,12 +2,16 @@ from p4a.fileimage.browser import ViewImage as P4AViewImage
 from p4a.fileimage import utils
 
 class ViewImage(P4AViewImage):
-    """A view for streaming image content.
+    """A view for streaming image content. This overrides the
+       p4a.fileimage.browser.ViewImage view because of flowplayer.
+       image urls must end with .jpg or .png, otherwise flowplayer
+       doesn't figure out what type of image it is, so we add the
+       extension to the url and remove it here.
     """
 
-    @property
-    def _tempfilename(self):
-        ifpackagename, ifname, fieldname = self.request.form.get('field', ':').split(':')
+    def __call__(self):
+        ifpackagename, ifname, fieldname = \
+                       self.request.form.get('field', ':').split(':')
         if fieldname.endswith('.jpg') or fieldname.endswith('.png'):
             fieldname = fieldname[:-4]
         ifpackage = __import__(ifpackagename, {}, {}, ifpackagename)
@@ -15,9 +19,4 @@ class ViewImage(P4AViewImage):
         adapted = iface(self.context)
         value = getattr(adapted, fieldname)
 
-        if value is None:
-            raise TypeError('Attribute for "%s.%s" should not be None'
-                            % (str(adapted), fieldname))
-
-        tempfilename = utils.write_ofsfile_to_tempfile(value)
-        return tempfilename
+        return value.index_html(self.request, self.request.response)
