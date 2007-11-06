fd0024f0fa3ad3046064bc161b35cc01cbff4b97
terletim <terletim@24821446-e7ea-0310-81ac-b189f4507b0b>
2007-11-06 15:29:00 +0000

    Added an 'Edit video metadata' tab for videos that are translated. The 'edit' tab shows the translation form, the 'Edit video metadata' tab shows the p4a form for changing size, width, duration etc. If the video isn't translated, then the 'edit' tab shows the p4a form. This fixes #5140.

--- trunk/eea.mediacentre/src/eea/mediacentre/browser/video.py
+++ trunk/eea.mediacentre/src/eea/mediacentre/browser/video.py
@@ -5,3 +5,17 @@ class VideoListedSingle(P4AVideoListedSingle):
     """Video listed single."""
 
     template = ViewPageTemplateFile('video-listed-single.pt')
+
+class VideoUtils(object):
+    """ A browser view for video utility methods. """
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def showEditMetadataTab(self):
+        if not self.context.isTranslatable():
+            return False
+
+        current = self.context.getLanguage()
+        return current != 'en'
