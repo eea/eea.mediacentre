ea2e4c6fd2d8719a7adf508da3a001097764d1e0
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-04-08 14:37:42 +0000

    Tagged eea.mediacentre 0.4.

---
+++ trunk/eea.mediacentre/tags/0.4/src/eea/mediacentre/browser/single.py
@@ -0,0 +1,51 @@
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getMultiAdapter
+from zope.interface import Interface
+from p4a.video.interfaces import IVideo
+
+class IListedSingle(Interface):
+    def single(obj=None, pos=None): #pylint: disable-msg = E0213
+        pass
+
+class ListedSingle(object):
+    """listed single."""
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def single(self, obj, pos=None, relevance=None):
+        video = IVideo(obj, None)
+        if video:
+            view = getMultiAdapter((self.context, self.request),
+                                   name='video_listed_single')
+            return view.single(video, pos, relevance)
+
+        if obj.portal_type == 'Image':
+            view = getMultiAdapter((self.context, self.request),
+                                   name='image_listed_single')
+            return view.single(obj, pos)
+
+        return ''
+
+
+class ImageListedSingle(object):
+
+    template = ViewPageTemplateFile('image-listed-single.pt')
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def single(self, obj=None, pos=None):
+        image = {
+            'title': obj.Title(),
+            'description': obj.Description(),
+            'url': obj.absolute_url() + '/view',
+            'media_types': 'Image',
+            'preview_url': obj.absolute_url() + '/image_mini',
+        }
+        if pos is not None:
+            image['oddeven'] = ['even', 'odd'][pos % 2]
+
+        return self.template(image=image)
