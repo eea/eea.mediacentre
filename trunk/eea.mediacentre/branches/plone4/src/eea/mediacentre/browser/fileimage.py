5c4e43021a6d79bf3ff1d5004ea19c05dff1de14
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-05-10 08:51:18 +0000

    Created Plone 4 branch, refs #8243

---
+++ trunk/eea.mediacentre/branches/plone4/src/eea/mediacentre/browser/fileimage.py
@@ -0,0 +1,22 @@
+from p4a.fileimage.browser import ViewImage as P4AViewImage
+#from p4a.fileimage import utils
+
+class ViewImage(P4AViewImage):
+    """A view for streaming image content. This overrides the
+       p4a.fileimage.browser.ViewImage view because of flowplayer.
+       image urls must end with .jpg or .png, otherwise flowplayer
+       doesn't figure out what type of image it is, so we add the
+       extension to the url and remove it here.
+    """
+
+    def __call__(self):
+        ifpackagename, ifname, fieldname = \
+                       self.request.form.get('field', ':').split(':')
+        if fieldname.endswith('.jpg') or fieldname.endswith('.png'):
+            fieldname = fieldname[:-4]
+        ifpackage = __import__(ifpackagename, {}, {}, ifpackagename)
+        iface = getattr(ifpackage, ifname)
+        adapted = iface(self.context)
+        value = getattr(adapted, fieldname)
+
+        return value.index_html(self.request, self.request.response)
---
+++ trunk/eea.mediacentre/branches/plone4/src/eea/mediacentre/browser/portlets/base.py
@@ -0,0 +1,53 @@
+from zope.component import getAdapter, getUtility
+from zope.interface import implements
+from eea.themecentre.browser.portlets.catalog import BasePortlet
+from eea.themecentre.themecentre import getTheme
+from eea.mediacentre.browser.interfaces import IMediaPortlet
+from Products.CMFPlone import utils
+from p4a.video.interfaces import IMediaPlayer
+#from p4a.video.browser.video import VideoPageView
+from eea.mediacentre.interfaces import IMediaCentre
+from eea.mediacentre.mediacentre import MEDIA_SEARCH_KEY
+
+class MediaPortlet(BasePortlet):
+    implements(IMediaPortlet)
+
+    def media_player(self):
+        """ Returns an html snippet for showing a video.
+            This works for the formats that p4avideo supports. """
+        #context = utils.context(self)
+        if self.items:
+            media_file = self.items[0]['object']
+            mime_type = media_file.get_content_type()
+            widget = getAdapter(media_file, IMediaPlayer, name=mime_type)
+            widget.use_height_only = True
+            return widget(None, None)
+        else:
+            return None
+
+    def item_to_short_dict(self, item):
+        return item
+
+    def item_to_full_dict(self, item):
+        return item
+
+    def all_link(self):
+        #mediacentre = getUtility(IMediaCentre)
+        #media_types = mediacentre.getMediaTypes()
+        template = self.media_type + 's'
+
+        context = utils.context(self)
+        return context.absolute_url() + '/' + template
+
+    def items(self):
+        context = utils.context(self)
+        theme = getTheme(context)
+        mediacentre = getUtility(IMediaCentre)
+        query= { MEDIA_SEARCH_KEY: {'theme': theme} }
+        files = mediacentre.getMedia(self.media_type, 1, search=query)
+        #self.items = files
+        return files
+
+    def short_items(self, media_type):
+        self.media_type = media_type
+        return super(MediaPortlet, self).short_items()
