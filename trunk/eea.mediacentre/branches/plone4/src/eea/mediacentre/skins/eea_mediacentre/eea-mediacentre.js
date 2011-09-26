e7542cbfd205c7d698a10b00df7efb432513d061
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-09-26 12:28:09 +0000

    Refs #8547; modified logic of fancybox pop-up to also take into account items that are not on page when this script is loaded, moved fancybox call to the first function, another each function is not needed

--- trunk/eea.mediacentre/branches/plone4/src/eea/mediacentre/skins/eea_mediacentre/eea-mediacentre.js
+++ trunk/eea.mediacentre/branches/plone4/src/eea/mediacentre/skins/eea_mediacentre/eea-mediacentre.js
@@ -2,12 +2,26 @@
 $(document).ready(function() {
 
     function prepareVideoLinkURLs() {
-        $('.video-fancybox').each(function() {
+        $("#content, #vids-slider").delegate(".video-fancybox", "hover", function(){
             var regex = /(\/$|\/view\/?$|\/video_popup_view\/?$)/;
-            var href = $(this).attr('href');
+            var href = this.href;
             href = href.replace(regex, ''); // remove any trailing '/view' or '/'
             href = href + "/video_popup_view";
-            $(this).attr('href', href);
+            this.href = href; 
+            var isInsidePopUp = $('body').hasClass('video_popup_view');
+            if (!isInsidePopUp) {
+                    $(this).fancybox({
+                        type: 'iframe',
+                        padding: 0,
+                        margin: 0,
+                        width: 675,
+                        height: 564,
+                        scrolling: 'no',
+                        autoScale: false,
+                        autoDimensions: false,
+                        titlePosition: 'wrap'
+                    });
+            }
         });
     }
     prepareVideoLinkURLs();
@@ -16,33 +30,15 @@ $(document).ready(function() {
         return;
     }
 
-    function prepareVideoLinks() {
-        var isInsidePopUp = $('body').hasClass('video_popup_view');
-        if (!isInsidePopUp) {
-            $('.video-fancybox').each(function() {
-                $(this).fancybox({
-                    type: 'iframe',
-                    padding: 0,
-                    margin: 0,
-                    width: 675,
-                    height: 564,
-                    scrolling: 'no',
-                    autoScale: false,
-                    autoDimensions: false,
-                    titlePosition: 'wrap'
-                });
-            });
-        }
-    }
-    prepareVideoLinks();
 
     if (window.Faceted) {
         jQuery(Faceted.Events).bind(Faceted.Events.AJAX_QUERY_SUCCESS, function(evt) {
             prepareVideoLinkURLs();
-            prepareVideoLinks();
+
         });
     }
 
 });
 
 })(jQuery);
+
