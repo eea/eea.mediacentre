b9dcc58ba2b1b05eecf2e1c2b64b1c6c11d62c5d
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-10-11 15:10:01 +0000

    Refs #8247; cleaned-up multimedia of inline styles; hid header and footer after widgets are loaded, in future will have button to toggle it back; cleaned stylesheet of old jqueryflip styles; WIP in adding the whatsnew gallery for videos, added white background with opacity for it;

--- trunk/eea.mediacentre/branches/plone4/src/eea/mediacentre/skins/eea_multimedia/eea.multimedia.js
+++ trunk/eea.mediacentre/branches/plone4/src/eea/mediacentre/skins/eea_multimedia/eea.multimedia.js
@@ -11,7 +11,10 @@
         bg2 = $("#background2"),
         content_flow = $("#contentFlow"),
         media_player = $("#media-player"),
-        media_flowplayer = $("#media-flowplayer");
+        media_flowplayer = $("#media-flowplayer"),
+        multimedia_widgets = $("#multimedia-widgets"),
+        top_widgets  = $("#top-widgets"),
+        bottom_widgets = $("#bottom-widgets");
         bg.hide();
         bg.fullBg();
         bg.fadeIn();
@@ -26,32 +29,34 @@
             $("#title").fadeOut(2000);
             /* $("#visual-portal-wrapper").animate({width:"1024px"},{queue:false, duration:1000, easing:'easeOutCirc'}); */
         }).delay(3000,function(){  
-            $("#multimedia-widgets").animate({height:"640px"},{queue:false, duration:500, easing:'easeOutCirc'});
+            multimedia_widgets.animate({height:"775px"},{queue:false, duration:500, easing:'easeOutCirc'});
         }).delay(3500,function(){  
-            $("#top-widgets").animate({top:"0px"},{queue:false, duration:1000, easing:'easeInOutBack'});
-            $("#bottom-widgets").animate({top:"430px"},{queue:false, duration:1000, easing:'easeInOutBack'});
+            $("#top-widgets, #bottom-widgets").animate({top:"0px"},{queue:false, duration:1000, easing:'easeInOutBack'});
+            $("#cross-site-top, #portal-header, #footer-wrapper, #portal-colophon").slideUp('fast');
         });
 
     function showMediaPlayer(item){
+            multimedia_widgets.animate({height:"+=65px"},{queue:false, duration:200, easing:'easeOutCirc'});
+            top_widgets.animate({height:"+=65px"},{queue:false, duration:200, easing:'easeOutCirc'});
         var thumb_url = item.content.getAttribute('src'),
             video_url = thumb_url.substring(0, thumb_url.length - 12);
         jQuery("#player-title").html(item.caption.innerHTML);
-        media_flowplayer.flashembed({
-            src:'++resource++flowplayer/flowplayer-3.2.2.swf'
+        media_flowplayer.flashembed(
+            {
+                src:'++resource++flowplayer/flowplayer-3.2.2.swf'
             },
             { 
                 config:{
-                clip: { 
-                'url' : video_url,
-                'autoBuffering': true,
-                'autoPlay': false,
-                'loop': false 
-                },
-                'useNativeFullScreen': true,
-                'initialScale': 'fit'
-            }
-        } 
-        );
+                    clip: { 
+                        'url' : video_url,
+                        'autoBuffering': true,
+                        'autoPlay': false,
+                        'loop': false 
+                    },
+                    'useNativeFullScreen': true,
+                    'initialScale': 'fit'
+                }
+            });
          
          bg2.attr('src', thumb_url.replace(/thumb/, "xlarge"));
          bg2.fullBg();
@@ -60,11 +65,13 @@
     }
 
     $("#fancybox-close").click(function(){
-        media_player.fadeOut('slow',function(){content_flow.fadeIn('slow',function(){});});    
+        media_player.fadeOut('fast',function(){content_flow.fadeIn('slow',function(){});});    
+        top_widgets.animate({height:"-=65px"},{queue: false, duration:400, easing:'easeOutCirc'});
+        multimedia_widgets.animate({height:"-=65px"},{queue: false, duration:400, easing:'easeOutCirc' });
         bg.fadeIn('slow',function(){bg2.fadeOut('slow',function(){
-            media_flowplayer.html("Please enable javascript or upgrade to <a href='http://www.adobe.com/go/getflashplayer'/> to watch the video.");
-            media_flowplayer.find('a').html("Flash 9");
-        });
+                media_flowplayer.html("Please enable javascript or upgrade to <a href='http://www.adobe.com/go/getflashplayer'/> to watch the video.");
+                media_flowplayer.find('a').html("Flash 9");
+            });
         });
     });
 
