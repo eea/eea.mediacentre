ec83d949b097d1b4f713fa5354b51820b34eda31
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-10-18 12:18:05 +0000

    Refs #8247; fixed fancybox popup for whatsnew gallery to make it scroll to the top where the other popup resides

--- trunk/eea.mediacentre/branches/plone4/src/eea/mediacentre/skins/eea_mediacentre/eea-mediacentre.js
+++ trunk/eea.mediacentre/branches/plone4/src/eea/mediacentre/skins/eea_mediacentre/eea-mediacentre.js
@@ -9,18 +9,32 @@ $(document).ready(function() {
             href = href + "/video_popup_view";
             this.href = href; 
             var isInsidePopUp = $('body').hasClass('video_popup_view');
+            var video_page = window.location.href.indexOf('videopage') != '-1' ? true : undefined;
+            var options = {
+                    type: 'iframe',
+                    padding: 0,
+                    margin: 0,
+                    width: 675,
+                    height: 564,
+                    scrolling: 'no',
+                    autoScale: false,
+                    autoDimensions: false,
+                    centerOnScroll : false
+            };
+            if (video_page) {
+                var mult = $("#multimedia-coverflow").offset();
+                options.width = 650;
+                options.onStart = function() {
+                    $.fancybox.center = function() { return false;};
+                    $('html, body').animate({scrollTop: 0}, 200);
+                    $("#fancybox-wrap").css({position : 'absolute'}).animate({
+                        left: mult.left - 18,
+                        top: mult.top - 18
+                    }, 200);   
+                };            
+            }
             if (!isInsidePopUp) {
-                    $(this).fancybox({
-                        type: 'iframe',
-                        padding: 0,
-                        margin: 0,
-                        width: 675,
-                        height: 564,
-                        scrolling: 'no',
-                        autoScale: false,
-                        autoDimensions: false,
-                        titlePosition: 'wrap'
-                    });
+                    $(this).fancybox( options );
             }
         });
     }
