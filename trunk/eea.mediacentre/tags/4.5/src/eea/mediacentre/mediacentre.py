14e1fbf81a5287135cc512c869a7b3ea4df83ac3
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-12-07 11:29:31 +0000

    Tagged eea.mediacentre 4.5.

---
+++ trunk/eea.mediacentre/tags/4.5/src/eea/mediacentre/mediacentre.py
@@ -0,0 +1,53 @@
+""" Media Centre
+"""
+from eea.mediacentre.interfaces import IMediaCentre, IMediaCentrePlugin
+from zope.interface import implements
+from zope.component import getAllUtilitiesRegisteredFor
+
+MEDIA_SEARCH_KEY = 'eea.mediacentre.search'
+
+class MediaCentre(object):
+    """ Media Centre
+    """
+    implements(IMediaCentre)
+
+    def getMedia(self, media_type=None, size=None,
+                 full_objects=True, search=None):
+        """ Get Media
+        """
+        if search is None:
+            search = {}
+        result = []
+        plugins = self._getPlugins()
+
+        for plugin in plugins:
+            mediaobjects = plugin.getMedia(media_type,
+                                           size,
+                                           full_objects,
+                                           search)
+            result.extend(mediaobjects)
+
+        return result
+
+    def getMediaTypes(self):
+        """ Get Media Types
+        """
+        mediatypes = {}
+        plugins = self._getPlugins()
+        for plugin in plugins:
+            mediatypes.update(plugin.getMediaTypes())
+
+        return mediatypes
+
+    def getPluginNames(self):
+        """ Get Plugin Names
+        """
+        plugins = self._getPlugins()
+        return [plugin.name for plugin in plugins]
+
+    def _getPlugins(self):
+        """ Get Plugins
+        """
+        plugins = getAllUtilitiesRegisteredFor(IMediaCentrePlugin)
+        
+        return plugins
