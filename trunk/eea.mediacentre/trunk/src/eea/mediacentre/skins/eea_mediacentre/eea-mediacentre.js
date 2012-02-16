b6a8abc0b2c10ec31cd30df6baa4c16eebb9b0c4
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-02-16 16:55:54 +0000

    Refs #8943; reduced the column size of the left side to 640px instead of 650px to have a perfect 16:9 size and to avoid small left and right background display; added a style of size for 4:3 which will make the 4:3 videos larger; switch scaling method to scale from fit since it fills the designated area better when having proper proportions of 4:3 and 16:9

--- trunk/eea.mediacentre/trunk/src/eea/mediacentre/skins/eea_mediacentre/eea-mediacentre.js
+++ trunk/eea.mediacentre/trunk/src/eea/mediacentre/skins/eea_mediacentre/eea-mediacentre.js
@@ -52,8 +52,10 @@
                     // if the parent can't find the img then we have listing view 
                     var thumb_url = src.length !== 0 ? src[0].src : $parent.closest('div').prev().children()[0].src;
 
-                    options.width = 650;
+                    options.width = 640;
                     options.height = 387;
+                    /* options.height = 507; for 4:3 */
+
                     options.overlayShow = false;
                     options.onStart = function() {
                         //close green tips media player if clicking on a video
