f23448ef6446eeac07b01089e5c2bea66f4ac159
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-03-06 08:43:13 +0000

    Refs #8947; added ability to add youtube playlist ids and have the playlist macro display the videos instead of having them hardcoded

--- trunk/eea.mediacentre/trunk/src/eea/mediacentre/browser/playlist.py
+++ trunk/eea.mediacentre/trunk/src/eea/mediacentre/browser/playlist.py
@@ -6,28 +6,25 @@ import json
 
 
 class VideoPlaylist(BrowserView):
-    """ Front page
+    """ BrowserView which returns youtube playlists
     """
-    #def __init__(self, context, request):
-    #    BrowserView.__init__(self, context, request)
 
     def playlist(self, playlist_id = None):
-        """ return video title id and description as well 
+        """ return video title, id and description as well
         as playlist title
         """
-        #items = []
-        if playlist_id:
-        #for obj in playlist_id:
+        items = []
+
+        if playlist_id and type(playlist_id) != list:
             url = 'http://gdata.youtube.com/feeds/api/playlists/' \
                     + playlist_id + '?v=2&alt=jsonc&orderby=position'
             link = urllib2.urlopen(url)
             playlist = json.load(link)
             playlist = playlist['data']
-            #playlist_title = playlist['title']
-            #playlist_list = []
-            #playlist_list.append(playlist_title)
-            #items.append(playlist_list)
-            #items.append(playlist_title)
+            playlist_title = playlist['title'] + ' (' \
+                                + str(playlist['totalItems']) + ')'
+            playlist_list = []
+            playlist_list.append(playlist_title)
             vids = playlist['items']
             vid_id = []
             vid_title = []
@@ -37,8 +34,34 @@ class VideoPlaylist(BrowserView):
                 vid_id.append(vid['id'])
                 vid_title.append(vid['title'])
                 vid_desc.append(vid['description'])
-            #items.append(vid_id)
-            #items.append(vid_title)
-            #items.append(vid_desc)
-        return (vid_id, vid_title, vid_desc)
-        #return items
+            playlist_list.append(vid_id)
+            playlist_list.append(vid_title)
+            playlist_list.append(vid_desc)
+            items.append(playlist_list)
+
+        if playlist_id and type(playlist_id) == list:
+            for obj in playlist_id:
+                url = 'http://gdata.youtube.com/feeds/api/playlists/' \
+                        + obj + '?v=2&alt=jsonc&orderby=position'
+                link = urllib2.urlopen(url)
+                playlist = json.load(link)
+                playlist = playlist['data']
+                playlist_title = playlist['title'] + ' (' \
+                                    + str(playlist['totalItems']) + ')'
+                playlist_list = []
+                playlist_list.append(playlist_title)
+                vids = playlist['items']
+                vid_id = []
+                vid_title = []
+                vid_desc = []
+                for i in vids:
+                    vid = i['video']
+                    vid_id.append(vid['id'])
+                    vid_title.append(vid['title'])
+                    vid_desc.append(vid['description'])
+                playlist_list.append(vid_id)
+                playlist_list.append(vid_title)
+                playlist_list.append(vid_desc)
+                items.append(playlist_list)
+
+        return items
