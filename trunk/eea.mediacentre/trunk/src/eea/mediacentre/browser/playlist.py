c6ba94ad5616919cd9fa0c62fc242fa4b8fdecb5
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-03-06 12:23:45 +0000

    Refs #8947;  just pass arguments to function instead of list which removes duplication code and makes it easier to cache the result of the function

--- trunk/eea.mediacentre/trunk/src/eea/mediacentre/browser/playlist.py
+++ trunk/eea.mediacentre/trunk/src/eea/mediacentre/browser/playlist.py
@@ -4,20 +4,19 @@ from Products.Five import BrowserView
 import urllib2
 import json
 
-
 class VideoPlaylist(BrowserView):
     """ BrowserView which returns youtube playlists
     """
 
-    def playlist(self, playlist_id = None):
+    def playlist(self, *args):
         """ return video title, id and description as well
         as playlist title
         """
         items = []
 
-        if playlist_id and type(playlist_id) != list:
+        for obj in args:
             url = 'http://gdata.youtube.com/feeds/api/playlists/' \
-                    + playlist_id + '?v=2&alt=jsonc&orderby=position'
+                    + obj + '?v=2&alt=jsonc&orderby=position'
             link = urllib2.urlopen(url)
             playlist = json.load(link)
             playlist = playlist['data']
@@ -38,30 +37,5 @@ class VideoPlaylist(BrowserView):
             playlist_list.append(vid_title)
             playlist_list.append(vid_desc)
             items.append(playlist_list)
-
-        if playlist_id and type(playlist_id) == list:
-            for obj in playlist_id:
-                url = 'http://gdata.youtube.com/feeds/api/playlists/' \
-                        + obj + '?v=2&alt=jsonc&orderby=position'
-                link = urllib2.urlopen(url)
-                playlist = json.load(link)
-                playlist = playlist['data']
-                playlist_title = playlist['title'] + ' (' \
-                                    + str(playlist['totalItems']) + ')'
-                playlist_list = []
-                playlist_list.append(playlist_title)
-                vids = playlist['items']
-                vid_id = []
-                vid_title = []
-                vid_desc = []
-                for i in vids:
-                    vid = i['video']
-                    vid_id.append(vid['id'])
-                    vid_title.append(vid['title'])
-                    vid_desc.append(vid['description'])
-                playlist_list.append(vid_id)
-                playlist_list.append(vid_title)
-                playlist_list.append(vid_desc)
-                items.append(playlist_list)
 
         return items
