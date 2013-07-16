aa24c90cd7df2d8b0b1718e1d70656625e0be1f1
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2013-07-16 15:27:45 +0000

    Refs #15234; removed duplication from upgrade steps that remove the p4a.* interfaces from video files

--- trunk/eea.mediacentre/branches/plone43/src/eea/mediacentre/upgrades/evolve56.py
+++ trunk/eea.mediacentre/branches/plone43/src/eea/mediacentre/upgrades/evolve56.py
@@ -8,73 +8,38 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def remove_video_enhanced_interfaces(ca):
-    """ Remove p4a.video.interfaces.IVideoEnhanced from objects
+def remove_interface(ca, interface):
+    """ Remove p4a.* from objects
     """
     count = 0
-    log.info("STARTED p4a.video.interfaces.IVideoEnhanced Removal")
+    iname = interface[0]
+    iface = interface[1]
+    log.info("STARTED %s Removal", iname)
     brains = ca.searchResults(object_provides=
-                              "p4a.video.interfaces.IVideoEnhanced",
+                              iname,
                               Language="all")
     for brain in brains:
         obj = brain.getObject()
-        noLongerProvides(obj, IVideoEnhanced)
+        try:
+            noLongerProvides(obj, iface)
+        except ValueError:
+            log.info("could remove inteface %s from %s", iface, obj)
+            continue
         obj.reindexObject(idxs=["object_provides"])
-        log.info("p4a.video.interfaces.IVideoEnhanced removed for %s",
-                 obj.absolute_url(1))
+        log.info("%s removed for %s", iname, obj.absolute_url(1))
         count += 1
         if count % 50 == 0:
             transaction.commit()
-    log.info("ENDED p4a.video.interfaces.IVideoEnhanced Removal")
+    log.info("ENDED %s Removal", iname)
 
 
 def cleanup_p4a_traces(context):
     """ Migration steps for getting rid of p4a.* interfaces
     """
     ca = context.portal_catalog
-    remove_video_container(ca)
-    remove_subtyper_interfaces(ca)
-    remove_video_enhanced_interfaces(ca)
-
-
-def remove_video_container(ca):
-    """ Remove p4a.video.interfaces.IVideoContainerEnhanced from objects
-    """
-    count = 0
-    log.info("STARTED p4a.video.interfaces.IVideoContainerEnhanced Removal")
-    brains = ca.searchResults(object_provides=
-                              "p4a.video.interfaces.IVideoContainerEnhanced",
-                              Language="all")
-    for brain in brains:
-        obj = brain.getObject()
-        noLongerProvides(obj, IVideoContainerEnhanced)
-        obj.reindexObject(idxs=["object_provides"])
-        log.info("p4a.video.interfaces.IVideoContainerEnhanced removed for %s",
-                 obj.absolute_url(1))
-        count += 1
-        if count % 50 == 0:
-            transaction.commit()
-    log.info("ENDED p4a.video.interfaces.IVideoContainerEnhanced Removal")
-
-
-def remove_subtyper_interfaces(ca):
-    """ Remove ISubtyper from objects that implement IVideoEnhanced
-    """
-    log.info("STARTED p4a.subtyper.interfaces.ISubtyped Removal")
-    count = 0
-    brains = ca.searchResults(object_provides=
-                              "p4a.video.interfaces.IVideoEnhanced",
-                              Language="all")
-    for brain in brains:
-        if "p4a.subtyper.interfaces.ISubtyped" in brain.object_provides:
-            obj = brain.getObject()
-            noLongerProvides(obj, ISubtyped)
-            obj.reindexObject(idxs=["object_provides"])
-            log.info("p4a.subtyper.interfaces.ISubtyped removed for %s",
-                     obj.absolute_url(1))
-            count += 1
-            if count % 50 == 0:
-                transaction.commit()
-    log.info("ENDED p4a.subtyper.interfaces.ISubtyped Removal")
-
-
+    interfaces = [("p4a.video.interfaces.IVideoEnhanced", IVideoEnhanced),
+                  ("p4a.video.interfaces.IVideoContainerEnhanced",
+                   IVideoContainerEnhanced),
+                  ("p4a.video.interfaces.ISubtyped", ISubtyped)]
+    for interface in interfaces:
+        remove_interface(ca, interface)
