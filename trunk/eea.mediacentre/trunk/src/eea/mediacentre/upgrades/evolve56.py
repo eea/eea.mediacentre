2e298d10db9f41aa17f1aeb8b358cefcd801ab31
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2013-08-05 14:23:12 +0000

    * Change: removed p4a.* interface removal from eea.mediacentre in order for this step to be done by Products.EEAPloneAdmin [ichimdav refs #15234]

--- trunk/eea.mediacentre/trunk/src/eea/mediacentre/upgrades/evolve56.py
+++ trunk/eea.mediacentre/trunk/src/eea/mediacentre/upgrades/evolve56.py
@@ -1,37 +1,9 @@
 """ p4a.* cleanup
 """
-from zope.interface import noLongerProvides
-from p4a.subtyper.interfaces import ISubtyped
-from p4a.video.interfaces import IVideoContainerEnhanced, IVideoEnhanced
 from zope.annotation import IAnnotations
 import transaction
 import logging
 log = logging.getLogger(__name__)
-
-
-def remove_interface(ca, interface):
-    """ Remove p4a.* from objects
-    """
-    count = 0
-    iname = interface[0]
-    iface = interface[1]
-    log.info("STARTED %s Removal", iname)
-    brains = ca.searchResults(object_provides=
-                              iname,
-                              Language="all")
-    for brain in brains:
-        obj = brain.getObject()
-        try:
-            noLongerProvides(obj, iface)
-        except ValueError:
-            log.info("could remove inteface %s from %s", iface, obj)
-            continue
-        obj.reindexObject(idxs=["object_provides"])
-        log.info("%s removed for %s", iname, obj.absolute_url(1))
-        count += 1
-        if count % 50 == 0:
-            transaction.commit()
-    log.info("ENDED %s Removal", iname)
 
 
 def set_fields_from_annotation(ca):
@@ -72,14 +44,6 @@ def cleanup_p4a_traces(context):
     """ Migration steps for getting rid of p4a.* interfaces
     """
     ca = context.portal_catalog
-    interfaces = [("p4a.video.interfaces.IVideoEnhanced", IVideoEnhanced),
-                  ("p4a.video.interfaces.IVideoContainerEnhanced",
-                   IVideoContainerEnhanced),
-                  ("p4a.video.interfaces.ISubtyped", ISubtyped)]
-
-    # remove interfaces
-    for interface in interfaces:
-        remove_interface(ca, interface)
 
     # set fields from annotation of objects
     set_fields_from_annotation(ca)
