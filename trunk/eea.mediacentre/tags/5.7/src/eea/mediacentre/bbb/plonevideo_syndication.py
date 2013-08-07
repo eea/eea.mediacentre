1e7c2d38d64ba4d93e8bf1377b0bc9ddf69c8861
szabozo0 <szabozo0@24821446-e7ea-0310-81ac-b189f4507b0b>
2013-08-07 11:45:38 +0000

    Tagged eea.mediacentre 5.7.

---
+++ trunk/eea.mediacentre/tags/5.7/src/eea/mediacentre/bbb/plonevideo_syndication.py
@@ -0,0 +1,98 @@
+""" p4a.plonevideo.syndication
+"""
+
+from zope import component
+from zope import interface
+from Products.fatsyndication import adapters as fatadapters
+from Products.basesyndication import interfaces as baseinterfaces
+from eea.mediacentre.interfaces import IVideo
+
+
+class VideoFeed(fatadapters.BaseFeed):
+    """ VideoFeed adapter
+    """
+    interface.implements(baseinterfaces.IFeed)
+    component.adapts(IVideo)
+
+
+class VideoFeedSource(fatadapters.BaseFeedSource):
+    """ VideoFeedSource
+    """
+    interface.implements(baseinterfaces.IFeedSource)
+    component.adapts(IVideo)
+
+    def getFeedEntries(self):
+        """See IFeedSoure
+        """
+        return [baseinterfaces.IFeedEntry(self.context)]
+
+
+
+class VideoFeedEntry(fatadapters.BaseFeedEntry):
+    """ VideoFeedEntry
+    """
+    interface.implements(baseinterfaces.IFeedEntry)
+    component.adapts(IVideo)
+
+    def __init__(self, context):
+        fatadapters.BaseFeedEntry.__init__(self, context)
+
+    def getBody(self):
+        """See IFeedEntry.
+        """
+        return ''
+
+    def getEnclosure(self):
+        """ getEnclosure
+        """
+        return baseinterfaces.IEnclosure(self.context)
+
+    def getTitle(self):
+        """ getTitle
+        """
+        return self.context.title
+
+    def getDescription(self):
+        """ getDescription
+        """
+        return self.context.description
+
+class ATFileEnclosure(object):
+    """ ATFileEnclosure
+    """
+    interface.implements(baseinterfaces.IEnclosure)
+    component.adapts(IVideo)
+
+    def __init__(self, context):
+        self.context = context
+        self.enclosure = self.context.getFile()
+
+    def getURL(self):
+        """ getURL
+        """
+        return self.context.absolute_url()
+
+    def getLength(self):
+        """ getLength
+        """
+        return self.enclosure.get_size()
+
+    def __len__(self):
+        return self.getLength()
+
+    def getMajorType(self):
+        """ getMajorType
+        """
+        return self.getType().split('/')[0]
+
+    def getMinorType(self):
+        """ getMinorType
+        """
+        return self.getType().split('/')[1]
+
+    def getType(self):
+        """ getType
+        """
+        return self.enclosure.getContentType()
+
+
